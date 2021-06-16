ca725623044e7e54030e5e12fce6d2eea95e8876
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-06-16 11:09:37 +0430

    add auth to allTAsks and getTAsk

--- start/routes/tasks.routes.ts
+++ start/routes/tasks.routes.ts
@@ -2,8 +2,8 @@ import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
   Route.post("/add", "TasksController.createTaks").middleware("auth:api");
-  Route.get("/all", "TasksController.allTasks");
-  Route.get("/:id", "TasksController.getTask");
+  Route.get("/all", "TasksController.allTasks").middleware("auth:api");
+  Route.get("/:id", "TasksController.getTask").middleware("auth:api");
   Route.put("/edit/:id", "TasksController.editTask").middleware("auth:api");
   Route.delete("/delete/:id", "TasksController.deleteTask").middleware(
     "auth:api"
